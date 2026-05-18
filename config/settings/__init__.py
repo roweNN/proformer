c12229798079bcd32fb5156dee04138921d82e85
Ronald Gonzalez <rowen@hotmail.cl>
2026-05-18 03:08:18 -0400

    Refactor settings initialization to load environment variables from .env file

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,5 +1,11 @@
 import os
+from pathlib import Path
 
+from dotenv import load_dotenv
+
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+load_dotenv(BASE_DIR / ".env")
 
 environment = os.getenv("DJANGO_ENV", "local").lower()
 
